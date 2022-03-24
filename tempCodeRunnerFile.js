251b4a514bc555fb99653cb507a2d7514d3e6da1
Jonathan Ho <ho.jonathan97@gmail.com>
2022-03-24 13:34:17 -0500

    Finished Adventure

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+if (backpack.length > 2) {
+   for (let i = 0; i < 2; i++) {
+       console.log (backpack[i])
+    } 
+}else {
+   for (let i = 0; i <backpack.length; i++) {
+     console.log(`This is the item at the index of ${i}:`,backpack[i])
+}
+}
